f29c194ffaddc90807a0c8c78af7dbb6c6e338ad
albertopercas <80323341+albertopercas@users.noreply.github.com>
2021-03-17 13:07:19 +0100

    Add files via upload

---
+++ Numeros2.java
@@ -0,0 +1,8 @@
+package ejercicios;
+
+public class Numeros2 {
+    public static void main(String[] args) {
+        //Usamos + y comillas para separar y unir varios
+       System.out.print("1" + "\n2" + "\n3" + "\n4" + "\n5");
+    }
+}
