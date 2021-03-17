493fd96cd07bb3361895df9453fc72beed766606
albertopercas <80323341+albertopercas@users.noreply.github.com>
2021-03-17 13:26:20 +0100

    Delete Numeros2.java

--- Numeros2.java
+++
@@ -1,8 +0,0 @@
-package ejercicios;
-
-public class Numeros2 {
-    public static void main(String[] args) {
-        //Usamos + y comillas para separar y unir varios
-       System.out.print("1" + "\n2" + "\n3" + "\n4" + "\n5");
-    }
-}
